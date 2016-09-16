858f7a0f23fea4fc43965c814422b1272ac77713
David Shea <dshea@redhat.com>
2016-09-16 17:03:03 -0400

    Be pickier about what tag data makes it to JSON.
    Modify RPM.Tags.Tag to derive Typeable and Data, and only output a tag's
    value if it is a string or an int.

--- rpm2json.hs
+++ rpm2json.hs
@@ -3,12 +3,16 @@
 {-# LANGUAGE OverloadedStrings #-}
 
 import           Conduit(($$), (=$), awaitForever, stdinC)
+import           Control.Monad(liftM)
+import           Control.Monad.Except(MonadError)
 import           Control.Monad.IO.Class(liftIO)
 import           Data.Aeson(Value(..), toJSON, ToJSON, object, (.=))
 import           Data.Aeson.TH(deriveToJSON, defaultOptions)
 import           Data.Aeson.Encode.Pretty(encodePretty)
 import qualified Data.ByteString.Lazy as BSL
 import           Data.Conduit(Conduit, Consumer, yield)
+import           Data.Data
+import           Data.Word
 import           RPM.Parse(parseRPMC)
 import           RPM.Tags(Tag)
 import           RPM.Types(RPM(..), Lead, SectionHeader, Header(..))
@@ -21,12 +25,51 @@ import           RPM.Types(RPM(..), Lead, SectionHeader, Header(..))
 deriveToJSON defaultOptions ''Lead
 deriveToJSON defaultOptions ''SectionHeader
 
--- for Tags, do basically what the pretty implementation does: split the show output into
--- words and return the first two. JSON-ize it as { "name" : "WhateverTag", "value" : "stuff goes here" }
+-- Tags, wow. What we want to see depends on what's in it. In general, the content is:
+--   - nothing at all.
+--   - a string. might be something good, show it!
+--   - an int. Probably don't care, but no harm in showing it anyway
+--   - a list of strings or ints which could well be three miles long. do not show these, holy crap
+--   - a bytestring, skip it
+-- JSON-ize as { "name" : "WhateverTag", "value" : "maybe a value" }
+
+-- first, some utility functions
+tagName :: Tag -> String
+tagName t = showConstr $ toConstr t
+
+-- This takes the first constructor parameter of the tag, which is where the data goes, and
+-- converts it to a TypeRep
+tagType :: Tag -> TypeRep
+tagType = gmapQi 0 typeOf
+
+-- Use a cast to pull the first parameter out of the constructor.
+tagValue :: (Typeable a) => Tag -> Maybe a
+tagValue = gmapQi 0 cast
+
+-- There's probably a better way to do this
+-- type needs to be explicit on account of OverloadedStrings
+stringType = typeOf ("" :: String)
+word32Type = typeOf (0 :: Word32)
+word64Type = typeOf (0 :: Word32)
+
 instance ToJSON Tag where
-    toJSON t = let tagParts = init . init $ words $ show t
-               in object [ "name"  .= (tagParts!!0),
-                           "value" .= (tagParts!!1) ]
+    toJSON t = let namePair = "name" .= tagName t
+                   -- Depending on the type of the thing, use a cast to pull
+                   -- it out of. Go ahead and conver to JSON here so everything
+                   -- is a common type.
+                   value    = case tagType t of
+                                stringType -> fmap toJSON (tagValue t :: Maybe String)
+                                word32Type -> fmap toJSON (tagValue t :: Maybe Word32)
+                                word64Type -> fmap toJSON (tagValue t :: Maybe Word64)
+                                _          -> Nothing
+                   -- If we have a value, it should be passed to the object below,
+                   -- otherwise just pass name.
+                   valueList = case value of
+                                Just x  -> [ "value" .= x ]
+                                Nothing -> []
+               in 
+               
+               object (namePair : valueList)
 
 -- for Header, skip the headerStore ByteStream
 instance ToJSON Header where
@@ -39,13 +82,13 @@ instance ToJSON RPM where
                           "rpmHeaders" .= toJSON (rpmHeaders rpm) ]
 
 -- conduit to encode RPM into a JSON value. Errors are passed through
-encodeC :: Monad m => Conduit (Either String RPM) m (Either String Value)
+encodeC :: MonadError e m => Conduit (Either e RPM) m (Either e Value)
 encodeC = awaitForever $ \case
     Left err  -> yield $ Left err
     Right rpm -> yield $ Right $ toJSON rpm
 
 -- output sink
-consumer :: Consumer (Either String Value) IO ()
+consumer :: Show e => Consumer (Either e Value) IO ()
 consumer = awaitForever $ \case
     Left err   -> liftIO $ print err
     Right json -> liftIO $ BSL.putStr $ encodePretty json
