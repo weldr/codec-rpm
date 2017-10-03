16203758f01cb1a322088fdcdc5d73ffb282fcf9
Chris Lumens <clumens@redhat.com>
2017-10-03 09:37:03 -0400

    Update the types of the various conduit functions.
    parseRPMC should return a ParseError instead of just a String.  That's a
    more useful error type.  It should continue using MonadError, though.
    The other functions should have their constraints relaxed a bit.  They
    don't do anything that involves an error condition.

--- Codec/RPM/Conduit.hs
+++ Codec/RPM/Conduit.hs
@@ -21,7 +21,7 @@ import           Control.Monad.Trans.Resource(MonadResource)
 import           Conduit((.|), Conduit, awaitForever, yield)
 import qualified Data.ByteString as BS
 import qualified Data.ByteString.Char8 as C
-import           Data.Conduit.Attoparsec(ParseError(..), conduitParserEither)
+import           Data.Conduit.Attoparsec(ParseError, conduitParserEither)
 import           Data.Conduit.Lzma(decompress)
 import           Data.CPIO(Entry, readCPIO)
 
@@ -39,19 +39,19 @@ import Codec.RPM.Types(RPM(..))
 -- On success, the 'RPM' record will be passed down the conduit for futher processing or
 -- consumption.  On error, the rest of the conduit will be skipped and the 'ParseError' will
 -- be returned as the result to be dealt with.
-parseRPMC :: MonadError String m => Conduit C.ByteString m RPM
+parseRPMC :: MonadError ParseError m => Conduit C.ByteString m RPM
 parseRPMC =
     conduitParserEither parseRPM .| consumer
  where
-    consumer = awaitForever $ either (throwError . errorMessage) (yield . snd)
+    consumer = awaitForever $ either throwError (yield . snd)
 
 -- | Extract the package payload from an 'RPM', returning it in the conduit.
-payloadC :: MonadError e m => Conduit RPM m BS.ByteString
+payloadC :: Monad m => Conduit RPM m BS.ByteString
 payloadC = awaitForever (yield . rpmArchive)
 
 -- | Extract the package payload from an 'RPM', decompress it, and return each element of
 -- the payload as a 'Data.CPIO.Entry'.
-payloadContentsC :: (MonadError e m, MonadResource m) => Conduit RPM m Entry
+payloadContentsC :: MonadResource m => Conduit RPM m Entry
 payloadContentsC = payloadC
                 .| decompress Nothing
                 .| readCPIO
