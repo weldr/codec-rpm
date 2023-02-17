408a829e587953fc78610ca747a6aea502f6929c
Christopher Sasarak <chris@fossa.com>
2023-02-17 15:52:56 -0600

    Clean up deprecation warnings for use of Conduit

--- Codec/RPM/Conduit.hs
+++ Codec/RPM/Conduit.hs
@@ -21,7 +21,7 @@ import           Control.Monad.Except(MonadError, throwError)
 import           Control.Monad.Trans.Resource(MonadResource)
 import qualified Data.ByteString as BS
 import qualified Data.ByteString.Char8 as C
-import           Data.Conduit((.|), Conduit, awaitForever, yield)
+import           Data.Conduit((.|), ConduitT, awaitForever, yield)
 import           Data.Conduit.Attoparsec(ParseError, conduitParserEither)
 import           Data.Conduit.Lzma(decompress)
 import           Data.CPIO(Entry, readCPIO)
@@ -40,19 +40,19 @@ import Codec.RPM.Types(RPM(..))
 -- On success, the 'RPM' record will be passed down the conduit for futher processing or
 -- consumption.  On error, the rest of the conduit will be skipped and the 'ParseError' will
 -- be returned as the result to be dealt with.
-parseRPMC :: MonadError ParseError m => Conduit C.ByteString m RPM
+parseRPMC :: MonadError ParseError m => ConduitT C.ByteString RPM m ()
 parseRPMC =
     conduitParserEither parseRPM .| consumer
  where
     consumer = awaitForever $ either throwError (yield . snd)
 
 -- | Extract the package payload from an 'RPM', returning it in the conduit.
-payloadC :: Monad m => Conduit RPM m BS.ByteString
+payloadC :: Monad m => ConduitT RPM BS.ByteString m ()
 payloadC = awaitForever (yield . rpmArchive)
 
 -- | Extract the package payload from an 'RPM', decompress it, and return each element of
 -- the payload as a 'Data.CPIO.Entry'.
-payloadContentsC :: (MonadResource m, MonadThrow m) => Conduit RPM m Entry
+payloadContentsC :: (MonadResource m, MonadThrow m) => ConduitT RPM Entry m ()
 payloadContentsC = payloadC
                 .| decompress Nothing
                 .| readCPIO
